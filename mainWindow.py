c79526d8802632ab2e32321fcf7e91c6b26cfcb5
Kevin Nguyen <darxtar0000@gmail.com>
2020-08-25 14:02:32 -0700

    Rework search bar to search with multiple tokens (supports exclusion)

--- mainWindow.py
+++ mainWindow.py
@@ -7,6 +7,7 @@ from rightPanel import RightPanel
 from tagManager import TagManager
 from videoImporter import VideoImporter
 from dbSchema import initializeDb
+from shlex import split
 
 '''
 Main Window UI contents
@@ -25,13 +26,14 @@ class MainWindow(QMainWindow):
         # Naming convenience
         self.videoTable = self.ui.videoTable
         self.tableSearchBar = self.ui.tableSearchBar
-        self.menubar = self.ui.menubar
+        self.menuBar = self.ui.menubar
         self.actionUpdate = self.ui.actionUpdate
         self.actionForceUpdate = self.ui.actionForceUpdate
         self.actionManageTags = self.ui.actionManageTags
         self.actionManagePlaylists = self.ui.actionManagePlaylists
         self.tableSortDropDown = self.ui.tableSortDropDown
         self.tableSortDirDropDown = self.ui.tableSortDirDropDown
+        self.statusBar = self.ui.statusbar
 
         # UI layout setup
         self.horizontalLayout = QHBoxLayout(self.ui.mainWidget)
@@ -83,6 +85,7 @@ class MainWindow(QMainWindow):
         self.lastUsedFilters = filters
         queries = []
         search = self.tableSearchBar.text()
+        tokens = split(search)
         query = QSqlQuery()
 
         if len(filters) == 0 and len(search) == 0:
@@ -111,25 +114,77 @@ class MainWindow(QMainWindow):
                                    WHERE video_id IN (\n""" + queryTemplate + "\n)"
                 queries.append(queryTemplate)
 
-            if len(search) > 0:
-                searchQuery = """SELECT video_id FROM videos WHERE channel_id IN (
-                                 SELECT channel_id FROM channels WHERE channel_title LIKE (?)
-                                 )
-                                 UNION
-                                 SELECT video_id FROM videos_playlists_link WHERE playlist_id IN (
-                                     SELECT playlist_id FROM playlists WHERE playlist_title LIKE (?)
-                                 )
-                                 UNION
-                                 SELECT video_id FROM videos WHERE video_title LIKE (?) OR description LIKE (?) OR notes LIKE (?)
-                                 UNION
-                                 SELECT video_id FROM videos_tags_link WHERE tag_name LIKE (?)
-                                 UNION
-                                 SELECT video_id FROM videos_yt_tags_link WHERE tag_name LIKE (?)"""
-                searchQuery = """SELECT video_id, video_title, publish_date, save_date, C.channel_title
-                                 FROM videos V INNER JOIN channels C
-                                 ON V.channel_id =C.channel_id
-                                 WHERE video_id IN (\n""" + searchQuery + "\n)"
+            if len(tokens) > 0:
+                posQuery = """
+                SELECT video_id FROM videos WHERE channel_id IN (
+                    SELECT channel_id FROM channels WHERE channel_title LIKE (?)
+                )
+                UNION
+                SELECT video_id FROM videos_playlists_link WHERE playlist_id IN (
+                   SELECT playlist_id FROM playlists WHERE playlist_title LIKE (?)
+                )
+                UNION
+                SELECT video_id FROM videos WHERE video_id LIKE (?) OR video_title LIKE (?) OR description LIKE (?) OR notes LIKE (?)
+                UNION
+                SELECT video_id FROM videos_tags_link WHERE tag_name LIKE (?)
+                UNION
+                SELECT video_id FROM videos_yt_tags_link WHERE tag_name LIKE (?)
+                """
+
+                posQuery = """
+
+                SELECT video_id, video_title, publish_date, save_date, C.channel_title
+                FROM videos V INNER JOIN channels C
+                ON V.channel_id =C.channel_id
+                WHERE video_id IN (\n""" + posQuery + "\n)\n"
+
+                negQuery = """
+                SELECT video_id FROM videos WHERE channel_id IN (
+                    SELECT channel_id FROM channels WHERE channel_title NOT LIKE (?)
+                )
+                INTERSECT
+                SELECT video_id FROM videos_playlists_link WHERE playlist_id IN (
+                   SELECT playlist_id FROM playlists WHERE playlist_title NOT LIKE (?)
+                )
+                INTERSECT
+                SELECT video_id FROM videos WHERE video_id NOT LIKE (?) AND video_title NOT LIKE (?)
+                INTERSECT
+                SELECT video_id FROM videos_tags_link WHERE tag_name NOT LIKE (?)
+                """
+
+                negQuery = """
+                SELECT video_id, video_title, publish_date, save_date, C.channel_title
+                FROM videos V INNER JOIN channels C
+                ON V.channel_id =C.channel_id
+                WHERE video_id IN (\n""" + negQuery + "\n)\n"
+
+                searchQuery = ""
+                for i, t in enumerate(tokens):
+                    if i != 0:
+                        searchQuery += "\nINTERSECT\n"
+                    subquery = posQuery if t[0] != '-' else negQuery
+                    searchQuery += subquery
                 queries.append(searchQuery)
+
+            # if len(search) > 0:
+                # searchQuery = """SELECT video_id FROM videos WHERE channel_id IN (
+                #                     SELECT channel_id FROM channels WHERE channel_title LIKE (?)
+                #                  )
+                #                  UNION
+                #                  SELECT video_id FROM videos_playlists_link WHERE playlist_id IN (
+                #                      SELECT playlist_id FROM playlists WHERE playlist_title LIKE (?)
+                #                  )
+                #                  UNION
+                #                  SELECT video_id FROM videos WHERE video_id LIKE (?) OR video_title LIKE (?) OR description LIKE (?) OR notes LIKE (?)
+                #                  UNION
+                #                  SELECT video_id FROM videos_tags_link WHERE tag_name LIKE (?)
+                #                  UNION
+                #                  SELECT video_id FROM videos_yt_tags_link WHERE tag_name LIKE (?)"""
+                # searchQuery = """SELECT video_id, video_title, publish_date, save_date, C.channel_title
+                #                  FROM videos V INNER JOIN channels C
+                #                  ON V.channel_id =C.channel_id
+                #                  WHERE video_id IN (\n""" + searchQuery + "\n)"
+                # queries.append(searchQuery)
 
             preQuery = "\nINTERSECT\n".join(queries) + f"\nORDER BY {tableSorts[self.tableSortDropDown.currentIndex()]} {sortDirs[self.tableSortDirDropDown.currentIndex()]}"
             query.prepare(preQuery)
@@ -138,9 +193,18 @@ class MainWindow(QMainWindow):
                 for filterName, categoryIndex in filters:
                     query.addBindValue(filterName)
 
-            if len(search) > 0:
-                for _ in range(7):
-                    query.addBindValue(f"%{search}%")
+            if len(tokens) > 0:
+                for t in tokens:
+                    if t[0] != '-':
+                        for _ in range(posQuery.count("(?)")):
+                            query.addBindValue(f"%{t}%")
+                    else:
+                        for _ in range(negQuery.count("(?)")):
+                            query.addBindValue(f"%{t[1:]}%")
+
+            # if len(search) > 0:
+                # for _ in range(8):
+                #     query.addBindValue(f"%{search}%")
 
         query.exec()
         self.videoTableQuery(query)
@@ -164,6 +228,7 @@ class MainWindow(QMainWindow):
 
         self.videoTable.selectionModel().selectionChanged.connect(self.videoTableSelectionChanged)  # Not placed under signals because selectionModel changes
         self.videoTableSelectionChanged()
+        self.statusBarUpdate()
 
     def videoTableSelectionChanged(self):
         '''
@@ -172,6 +237,7 @@ class MainWindow(QMainWindow):
         # update preview panel with currentIndex()
         self.leftPanel.tagListUpdate()
         self.rightPanel.panelUpdate()
+        self.statusBarUpdate()
 
     def getVideoTableSelectionVideoIds(self):
         return [index.data() for index in self.videoTable.selectionModel().selectedRows(0)]
@@ -181,20 +247,36 @@ class MainWindow(QMainWindow):
 
     def getVideoTableCurrentVideo(self):
         # rewrite to allow for table custom view
-        video_id = self.videoTable.selectionModel().currentIndex().siblingAtColumn(0).data()
-        query = QSqlQuery()
-        query.prepare("SELECT * FROM videos WHERE video_id = (?)")
-        query.addBindValue(video_id)
+        if self.videoTableHasSelection():
+            video_id = self.videoTable.selectionModel().currentIndex().siblingAtColumn(0).data()
+            query = QSqlQuery()
+            query.prepare("SELECT * FROM videos WHERE video_id = (?)")
+            query.addBindValue(video_id)
+            query.exec()
+            query.first()
+            video = [query.value(0),
+                     query.value(1),
+                     query.value(2),
+                     query.value(3),
+                     query.value(4),
+                     query.value(5),
+                     query.value(6)]
+        else:
+            video = [None] * 7
+        return video
+
+    def statusBarUpdate(self):
+        displayedVids = self.videoTable.model().rowCount()
+        totalVids = 0
+        query = QSqlQuery("SELECT count(*) FROM videos")
         query.exec()
         query.first()
-        video = [query.value(0),
-                 query.value(1),
-                 query.value(2),
-                 query.value(3),
-                 query.value(4),
-                 query.value(5),
-                 query.value(6)]
-        return video
+        totalVids = query.value(0)
+        msg = f"[{displayedVids} of {totalVids} videos"
+        if self.videoTableHasSelection():
+            msg += f", {len(self.videoTable.selectionModel().selectedRows(0))} selected"
+        msg += "]"
+        self.statusBar.showMessage(msg)    
 
     def update(self):
         self.importer.importVideosFromAllPlaylists(refresh=False, update=True)
